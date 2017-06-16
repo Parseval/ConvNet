d765e97c65e0405a3a96e01605698e1f8d85c6ea
Parseval Team <Parseval Team>
2017-06-16 15:53:09 -0400

    Dir traversal up; file loading up

--- create-data.py
+++ create-data.py
@@ -4,42 +4,66 @@ Created on Thu Jun 15 20:22:49 2017
 
 @author: Steven
 """
-import soundfile as sf
 import numpy as np
-import glob
 import os
-import path
 
 data = []; #init global data var
-samplerate = 0;#init global sample rate var
+samplerate = 16000; #init global sample rate var
+dir_path = os.path.dirname(os.path.realpath(__file__))
 type_raw = '*.raw';
-test_path = './DATA/TEST'
+test_path = os.path.join(dir_path, "DATA", "TEST")
+train_path = os.path.join(dir_path, "DATA", "TRAIN")
+interval = 200
 
 #Smoke Ganja
 
 #load raw file
 def load_raw(file):
-    data, sampleRate = sf.read(file)
+    data = []
+    print("File: " + file + " loading")
+    data = np.fromfile(file)
+    print("File: " + file + " loaded")
+    print(len(data))
  
 #Chop, Chop (chop, chop)
 #20 second clips
-def chop_block(file, interval):
-    return chunks = [file[i:i+interval] for i in range(0, len(file), interval)]
+def chop_block():
     
-def main(file_name, starting_value):
-    file_name = file_name
-    starting_value = starting_value
-    training_data = []
+    chunks = np.array_split(data, interval)
+    print(len(chunks))
     
-    for filename in glob.glob(os.path.join(path, type_raw)):
-        #iterates through directory "path" for files of type raw
-        load_raw(os.path.join(os.path.join(path, filename)))
-        
-        if len(training_data) == 500:
-                        np.save(file_name,training_data)
-                        print('SAVED')
-                        #print(training_data)
-                        training_data = []
-                        starting_value += 1
-                        file_name = './DATA/PROCESSED/TRAINING_DATA-{}.npy'.format(starting_value)
+    i = 0
+    for chunk in chunks:
+        print(chunk)
+        i+=1
+    return chunks
     
+def main():
+    #file_name = file_name
+    #starting_value = starting_value
+    #training_data = []
+    print("Initialize; finding files")
+    print(dir_path)
+    print(test_path)
+    print(train_path)
+    
+    for root, dirs, files in os.walk(train_path):
+        for file in files:
+            if file.endswith('.raw'):
+                #print("files:")
+                #print (root + "\\" +  file)
+                load_raw(os.path.join(os.path.join(root, file)))
+                chunks = chop_block()
+
+    #print("File search complete.")
+    
+        #if len(training_data) >= 10000:
+         #               np.save(file_name,training_data)
+          #              print('SAVED')
+           #             #print(training_data)
+            #            training_data = []
+             #           starting_value += 1
+              #          file_name = './DATA/PROCESSED/TRAINING_DATA-{}.npy'.format(starting_value)
+              
+              
+main()
