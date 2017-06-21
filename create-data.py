9fdc98417859fb5c997bdeb0695b0e9e590fd20b
Steven Schweiger <sschweig90@gmail.com>
2017-06-21 16:34:06 -0400

    create-data.py functional

--- create-data.py
+++ create-data.py
@@ -7,7 +7,8 @@ Created on Thu Jun 15 20:22:49 2017
 import numpy as np
 import os
 
-data = []; #init global data var
+starting_value = 0
+
 samplerate = 16000; #init global sample rate var
 dir_path = os.path.dirname(os.path.realpath(__file__))
 type_raw = '*.raw';
@@ -15,55 +16,57 @@ test_path = os.path.join(dir_path, "DATA", "TEST")
 train_path = os.path.join(dir_path, "DATA", "TRAIN")
 interval = 200
 
-#Smoke Ganja
+#Consider harvesting youtube videos to increase training data. Words arent 
+#really important so long as I can label the independent speakers.
+
+#This script will roll through /DATA/TRAIN and process files for use.
+#File structure should be: /DATA/TRAIN/XXXX/YYYY WHERE:
+#XXXX = a single source (person)
+#YYYY = individual sample sources
+
+#The resulting training data files will consist of a Numpy array of size 10
+#Each index will represent an individual and will contain another array
+#This second level array will contain the various sources for that individual
 
 #load raw file
 def load_raw(file):
-    data = []
-    print("File: " + file + " loading")
-    data = np.fromfile(file)
-    print("File: " + file + " loaded")
-    print(len(data))
- 
-#Chop, Chop (chop, chop)
-#20 second clips
-def chop_block():
-    
-    chunks = np.array_split(data, interval)
-    print(len(chunks))
-    
-    i = 0
-    for chunk in chunks:
-        print(chunk)
-        i+=1
-    return chunks
+    file_data = []
+    file_data = np.fromfile(file)
+    return file_data
     
 def main():
-    #file_name = file_name
-    #starting_value = starting_value
-    #training_data = []
+    training_data = []
+    dir_data = []
+    starting_value = 0
+    file_name = './DATA/PROCESSED/TRAINING_DATA-{}.npy'.format(starting_value)
     print("Initialize; finding files")
     print(dir_path)
     print(test_path)
     print(train_path)
     
     for root, dirs, files in os.walk(train_path):
-        for file in files:
+        for index, file in enumerate(files):
             if file.endswith('.raw'):
-                #print("files:")
-                #print (root + "\\" +  file)
-                load_raw(os.path.join(os.path.join(root, file)))
-                chunks = chop_block()
-
-    #print("File search complete.")
-    
-        #if len(training_data) >= 10000:
-         #               np.save(file_name,training_data)
-          #              print('SAVED')
-           #             #print(training_data)
-            #            training_data = []
-             #           starting_value += 1
-              #          file_name = './DATA/PROCESSED/TRAINING_DATA-{}.npy'.format(starting_value)
-              
-              
+                if index == 0:
+                    print('----NEW DIRECTORY-----')
+                    training_data.append(dir_data)
+                    dir_data = []
+                                        
+                    if len(training_data) == 10:
+                        np.save(file_name, training_data)
+                        print('SAVED {}'.format(starting_value))
+                        #print(training_data)
+                        training_data = []
+                        starting_value += 1
+                        file_name = './DATA/PROCESSED/TRAINING_DATA-{}.npy'.format(starting_value)
+               
+                
+                dir_data.append(load_raw(os.path.join(os.path.join(root, file))))
+                
+                
+    training_data.append(dir_data)
+    np.save(file_name, training_data)
+    print('SAVED {}'.format(starting_value))
+    print('Done. Closing.')
+                           
 main()
