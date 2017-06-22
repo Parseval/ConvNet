65392c97c03de6554cae0084af31fde27050ef7a
Steven Schweiger <sschweig90@gmail.com>
2017-06-22 14:10:25 -0400

    Spec in progress

--- create-data.py
+++ create-data.py
@@ -6,15 +6,24 @@ Created on Thu Jun 15 20:22:49 2017
 """
 import numpy as np
 import os
+from matplotlib import pyplot as plt
+from numpy.lib import stride_tricks
+import wave as wav
+from matplotlib.pyplot import specgram
 
 starting_value = 0
 
-samplerate = 16000; #init global sample rate var
 dir_path = os.path.dirname(os.path.realpath(__file__))
 type_raw = '*.raw';
 test_path = os.path.join(dir_path, "DATA", "TEST")
 train_path = os.path.join(dir_path, "DATA", "TRAIN")
-interval = 200
+chunk_size = 1024
+samplerate = 16000; #init global sample rate var
+cliptime = 5
+bitspersample = 16
+fft_size = 512
+overlap_fac = 0
+
 
 #Consider harvesting youtube videos to increase training data. Words arent 
 #really important so long as I can label the independent speakers.
@@ -27,18 +36,41 @@ interval = 200
 #The resulting training data files will consist of a Numpy array of size 10
 #Each index will represent an individual and will contain another array
 #This second level array will contain the various sources for that individual
+#Seperated into chunk_size chunks
+
+def raw_to_wav(file, name, index):
+    sep = '.'
+    rest = name.split(sep, 1)[0]
+    file_data = np.fromfile(file, dtype='uint8')
+    #print(file_data)
+    with wav.open('wav_{}_{}.wav'.format(rest, index), 'wb') as wavfile:
+        print('{}'.format(index))
+        wavfile.setparams((1, 2, samplerate, 0, 'NONE', 'NONE'))
+        wavfile.writeframes(file_data)
+    #return wavfile
 
 #load raw file
 def load_raw(file):
     file_data = []
-    file_data = np.fromfile(file)
-    return file_data
+    chunk_data = np.random.rand(chunk_size)
+    #file_data = np.float32(file_data)
+    file_data = np.fromfile(file, dtype='uint8')
+    chunk_data = [file_data[x:x+chunk_size] for x in range(0, len(file_data), chunk_size)]
+    #print(chunk_data)
+    #print(file)
+    return chunk_data
+
+def load_wav(file):
+    with wav.open(file, 'rb') as wavfile:
+        return wavfile
     
 def main():
     training_data = []
     dir_data = []
-    starting_value = 0
-    file_name = './DATA/PROCESSED/TRAINING_DATA-{}.npy'.format(starting_value)
+    specs = []
+    #starting_value = 0
+    #file_name = './DATA/PROCESSED/TRAINING_DATA-{}.npy'.format(starting_value)
+    np.set_printoptions(threshold=np.inf)
     print("Initialize; finding files")
     print(dir_path)
     print(test_path)
@@ -51,7 +83,7 @@ def main():
                     print('----NEW DIRECTORY-----')
                     training_data.append(dir_data)
                     dir_data = []
-                                        
+                    """
                     if len(training_data) == 10:
                         np.save(file_name, training_data)
                         print('SAVED {}'.format(starting_value))
@@ -59,14 +91,24 @@ def main():
                         training_data = []
                         starting_value += 1
                         file_name = './DATA/PROCESSED/TRAINING_DATA-{}.npy'.format(starting_value)
-               
+                        """
                 
-                dir_data.append(load_raw(os.path.join(os.path.join(root, file))))
-                
-                
-    training_data.append(dir_data)
-    np.save(file_name, training_data)
-    print('SAVED {}'.format(starting_value))
+                chunks = load_raw(os.path.join(os.path.join(root, file)))
+                for chunk in chunks:
+                    spec = chunk.specgram( NFFT=128, Fs=samplerate, noverlap=64)
+                    show()
+                    plot = spec.plot()
+                    #print(spec)
+                    for s in spec:
+                        print(s)
+                        s.plot()
+                        input("Enter...........")
+                #plotstft(wavy)
+                #dir_data.append(load_raw(os.path.join(os.path.join(root, file))))
+            
+    #training_data.append(dir_data)
+    #np.save(file_name, training_data)
+    #print('SAVED {}'.format(starting_value))
     print('Done. Closing.')
                            
 main()
--- test.py
+++ test.py
@@ -4,14 +4,42 @@ Created on Thu Jun 15 20:22:49 2017
 
 @author: Steven
 """
-import soundfile as sf
+import numpy as np
+import sys
+import os
+import cntk as C
 
-data = []; #init global data var
-sampleRate = 0;#inite global sample rate var
+abs_path   = os.path.dirname(os.path.abspath(__file__))
+data_path  = os.path.join(abs_path, "Data")
+features_file = os.path.join(data_path, 'glob_0000.scp')
+labels_file = os.path.join(data_path, 'glob_0000.mlf')
+label_mapping_file = os.path.join(data_path, 'state.list')
+outputdir = os.path.join(abs_path, "OUTPUT")
+logdir = os.path.join(abs_path, "LOGS")
+num_output_classes = 5
 
-#load raw file
-def load_raw(file):
-    data, sampleRate = sf.read(file)
-    
+C.device.try_set_default_device(C.device.cpu())
 
-    
+# Ensure we always get the same amount of randomness
+np.random.seed(0)
+
+# Define the data dimensions
+input_dim_model = (1, 200, 1)    # images are 28 x 28 with 1 channel of color (gray)
+input_dim = 200*1                # used by readers to treat input data as a vector
+num_output_classes = 5
+
+
+# Define the reader for both training and evaluation action.
+def create_reader(path, is_training, input_dim, label_dim):
+    return C.io.MinibatchSource(C.io.CTFDeserializer(path, C.io.StreamDefs(
+        features=C.io.StreamDef(field='features', shape=input_dim),
+        labels=C.io.StreamDef(field='labels',   shape=label_dim)
+    )), randomize=is_training, max_sweeps=C.io.INFINITELY_REPEAT if is_training else 1)
+
+
+def main():
+    print("test")
+    # Load test data
+    reader_test = create_reader(os.path.join(data_path, 'Test-28x28_cntk_text.txt'), False, input_dim, num_output_classes)
+   
+main()    
